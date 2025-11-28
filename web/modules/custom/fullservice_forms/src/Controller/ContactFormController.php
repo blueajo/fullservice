e94dbff299c0f00acbfcaf1012c4d37b26a14873
Blue Jo <blueajo@gmail.com>
2025-11-28 18:13:27 -0500

    email patch

--- web/modules/custom/fullservice_forms/src/Controller/ContactFormController.php
+++ web/modules/custom/fullservice_forms/src/Controller/ContactFormController.php
@@ -6,33 +6,37 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class ContactFormController extends ControllerBase {
+class ContactFormController extends ControllerBase
+{
 
-  public function submit(Request $request) {
+  public function submit(Request $request)
+  {
     $data = json_decode($request->getContent(), TRUE);
-    
+
     // Validate
     if (empty($data['from']) || empty($data['subject']) || empty($data['message'])) {
       return new JsonResponse(['error' => 'Missing required fields'], 400);
     }
-    
+
     // Send email
     $mailManager = \Drupal::service('plugin.manager.mail');
     $module = 'fullservice_forms';
     $key = 'contact_form';
-    $to = \Drupal::config('system.site')->get('mail') ?: 'info@fullserviceoffice.com';
-    $params['message'] = $data['message'];
-    $params['from'] = $data['from'];
+    $to = 'info@fullserviceoffice.com';
     $params['subject'] = $data['subject'];
+    $params['message'] = $data['message'];
     $params['pitches'] = $data['pitches'] ?? [];
+    $params['reply_to'] = $data['from']; // Set reply-to to user email
+
     $langcode = \Drupal::currentUser()->getPreferredLangcode();
-    
-    $result = $mailManager->mail($module, $key, $to, $langcode, $params, $data['from']);
-    
+    $from_email = 'noreply@fullserviceoffice.com'; // Must match SMTP
+
+    $result = $mailManager->mail($module, $key, $to, $langcode, $params, $from_email);
+
     if ($result['result']) {
       return new JsonResponse(['success' => TRUE, 'message' => 'Email sent!']);
     }
-    
+
     return new JsonResponse(['error' => 'Failed to send email'], 500);
   }
 }
